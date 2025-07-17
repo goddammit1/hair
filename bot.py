8764c7e88483298231ade29992756f6b79b5428d
goddammit1 <164378662+goddammit1@users.noreply.github.com>
2025-07-17 16:35:02 +0300

    overload fixed(пиздежь)

--- bot.py
+++ bot.py
@@ -1,8 +1,9 @@
 import telebot
 from telebot import types
-from config import TOKEN  # Импортируем токен из config.py
+from config import TOKEN
 from image_handler import get_image_paths, send_image
 from jsonreader import read_json
+from constants import param_types, next_param, step_data, param_dependencies
 
 # Инициализируем бота
 bot = telebot.TeleBot(TOKEN)
@@ -10,54 +11,52 @@ bot = telebot.TeleBot(TOKEN)
 # Читаем данные из JSON-файла
 hairstyles_data = read_json('hair.json')
 
-# Функция для получения списка доступных вариантов для параметра
-def get_options(param, current_selection=None):
-    if param == 'face_type':
-        return list(hairstyles_data.keys())
-    elif param == 'hair_length' and current_selection and 'face' in current_selection:
-        return list(hairstyles_data[current_selection['face']].keys())
-    elif param == 'hair_type' and current_selection and 'hair_length' in current_selection:
-        return list(hairstyles_data[current_selection['face']][current_selection['hair_length']].keys())
-    elif param == 'hair_color' and current_selection and 'hair_type' in current_selection:
-        return list(hairstyles_data[current_selection['face']][current_selection['hair_length']][current_selection['hair_type']].keys())
-    return []
+# Глобальный словарь для хранения состояния
+selection_store = {}
 
-# Функция для создания клавиатуры с кнопками
+# Функция для получения вариантов выбора
+def get_options(param_type, selection):
+    dependencies = param_dependencies[param_type]
+    data = hairstyles_data
+    for dep in dependencies:
+        if dep not in selection:
+            return []
+        data = data[selection[dep]]
+    return list(data.keys())
+
+# Функция для создания клавиатуры
 def create_keyboard(options, callback_prefix):
     keyboard = types.InlineKeyboardMarkup()
     for option in options:
         keyboard.add(types.InlineKeyboardButton(text=option, callback_data=f"{callback_prefix}_{option}"))
     return keyboard
 
-# Глобальный словарь для хранения состояния выбора по chat_id
-selection_store = {}  # chat_id -> selection_dict
+# Функция для отправки фото с клавиатурой
+def send_selection_photo(chat_id, image_path, caption, options, callback_prefix):
+    keyboard = create_keyboard(options, callback_prefix)
+    with open(image_path, 'rb') as image_file:
+        bot.send_photo(chat_id, photo=image_file, caption=caption, reply_markup=keyboard)
 
 # Обработчик команды /start
 @bot.message_handler(commands=['start'])
 def start(message):
     chat_id = message.chat.id
-    selection_store[chat_id] = {}  # Сбрасываем состояние выбора
-    face_types = get_options('face_type')
-    if not face_types:
+    selection_store[chat_id] = {}
+    options = get_options('face', {})
+    if not options:
         bot.send_message(chat_id, "Ошибка: данные о прическах не найдены!")
         return
-    keyboard = create_keyboard(face_types, 'face')
-    with open('assets/1.jpg', 'rb') as image_file:
-        bot.send_photo(chat_id, photo=image_file, caption="Выберите форму лица:", reply_markup=keyboard)
-
-known_param_types = ['face', 'hair_length', 'hair_type', 'hair_color']
+    send_selection_photo(chat_id, step_data['face']['image'], step_data['face']['caption'], options, 'face')
 
 # Обработчик callback-запросов
 @bot.callback_query_handler(func=lambda call: True)
 def callback_query(call):
     chat_id = call.message.chat.id
-    # Инициализируем состояние для chat_id, если его нет
     if chat_id not in selection_store:
         selection_store[chat_id] = {}
     selection = selection_store[chat_id]
     
-    # Парсим данные callback
-    for param in known_param_types:
+    for param in param_types:
         if call.data.startswith(param + '_'):
             param_type = param
             selected_value = call.data[len(param) + 1:]
@@ -66,53 +65,33 @@ def callback_query(call):
         bot.answer_callback_query(call.id, "Неверный формат данных!")
         return
     
-    # Обновляем состояние выбора
     selection[param_type] = selected_value
     print(f"Текущее состояние для chat {chat_id}: {selection}")
     
-    # Логика для каждого шага
-    if param_type == 'face':
-        hair_lengths = get_options('hair_length', selection)
-        if not hair_lengths:
-            bot.send_message(chat_id, "Ошибка: длины волос не найдены!")
+    next_param_type = next_param.get(param_type)
+    if next_param_type:
+        options = get_options(next_param_type, selection)
+        if not options:
+            bot.send_message(chat_id, f"Ошибка: варианты для {next_param_type} не найдены!")
+            bot.answer_callback_query(call.id)
             return
-        keyboard = create_keyboard(hair_lengths, 'hair_length')
-        with open('assets/2.jpg', 'rb') as image_file:
-            bot.send_photo(chat_id, photo=image_file, caption="Выберите длину волос:", reply_markup=keyboard)
-    
-    elif param_type == 'hair_length':
-        hair_types = get_options('hair_type', selection)
-        if not hair_types:
-            bot.send_message(chat_id, "Ошибка: типы волос не найдены!")
-            return
-        keyboard = create_keyboard(hair_types, 'hair_type')
-        with open('assets/3.jpg', 'rb') as image_file:
-            bot.send_photo(chat_id, photo=image_file, caption="Выберите тип волос:", reply_markup=keyboard)
-    
-    elif param_type == 'hair_type':
-        hair_colors = get_options('hair_color', selection)
-        if not hair_colors:
-            bot.send_message(chat_id, "Ошибка: цвета волос не найдены!")
-            return
-        keyboard = create_keyboard(hair_colors, 'hair_color')
-        with open('assets/4.jpg', 'rb') as image_file:
-            bot.send_photo(chat_id, photo=image_file, caption="Выберите цвет волос:", reply_markup=keyboard)
-    
-    elif param_type == 'hair_color':
-        # Все параметры выбраны, отправляем изображения
+        image_path = step_data[next_param_type]['image']
+        caption = step_data[next_param_type]['caption']
+        send_selection_photo(chat_id, image_path, caption, options, next_param_type)
+    else:
         face_type = selection['face']
         hair_length = selection['hair_length']
         hair_type = selection['hair_type']
-        hair_color = selected_value
+        hair_color = selection['hair_color']
         image_paths = get_image_paths(hairstyles_data, face_type, hair_length, hair_type, hair_color)
         if image_paths:
             send_image(bot, chat_id, image_paths)
-            # Опционально: удаляем предыдущее сообщение
-            # bot.delete_message(chat_id, call.message.message_id)
         else:
             bot.send_message(chat_id, "Извини, такого изображения нет!")
+    
+    bot.answer_callback_query(call.id)
 
-# Запускаем бота
+# Запуск бота
 if __name__ == '__main__':
     try:
         bot.polling(none_stop=True)
---
+++ constants.py
@@ -0,0 +1,20 @@
+# Определяем параметры и их зависимости
+param_types = ['face', 'hair_length', 'hair_type', 'hair_color']
+next_param = {
+    'face': 'hair_length',
+    'hair_length': 'hair_type',
+    'hair_type': 'hair_color',
+    'hair_color': None
+}
+step_data = {
+    'face': {'image': 'assets/1.jpg', 'caption': 'Выберите форму лица:'},
+    'hair_length': {'image': 'assets/2.jpg', 'caption': 'Выберите длину волос:'},
+    'hair_type': {'image': 'assets/3.jpg', 'caption': 'Выберите тип волос:'},
+    'hair_color': {'image': 'assets/4.jpg', 'caption': 'Выберите цвет волос:'}
+}
+param_dependencies = {
+    'face': [],
+    'hair_length': ['face'],
+    'hair_type': ['face', 'hair_length'],
+    'hair_color': ['face', 'hair_length', 'hair_type']
+}
